8641a1e5bf7fe81ca02dc09b5e7ce2f4971df5da
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-13 14:59:07 -0500

    commented all functions

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -3,14 +3,19 @@ let DOM = {};
 let cardFrame = document.getElementById('card-frame');
 
 function writeToDOM(data, id) {
+	//simplify object keys
 	let productsObj = data.products.products,
 	typesObj = data.types.types,
 	categoriesObj = data.categories.categories,
+	//create arrays to hold data for each product
 	productsArr = [], descriptionsArr = [], typesArr = [],
 	categoriesArr = [];
+	//create containers to write data to the DOM, clear pre-existing data
 	let content = '';
+	let contentDiv = document.createElement('div');
 	cardFrame.innerHTML = '';
 
+	//create arrays for name, type, category, and description of each item
 	productsObj.forEach((product) => {
 		for (var key in product) {
 			if (product[key].id == id){
@@ -21,7 +26,7 @@ function writeToDOM(data, id) {
 			}
 		}
 	});
-	let contentDiv = document.createElement('div');
+	//create HTML for each product`
 	for (let i = 0; i < productsArr.length; i++) {
 		content += `
 				<div class="col-sm-3 card">
@@ -32,10 +37,12 @@ function writeToDOM(data, id) {
 				</div>
 		`;
 	}
+	//append HTML content to card Frame
 	contentDiv.innerHTML = content;
 	cardFrame.appendChild(contentDiv);
 }
 
+//loadData passes data to writeToDOM function
 DOM.loadData = (data, id)=> {
 	writeToDOM(data, id);
 };
