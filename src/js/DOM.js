0b95c16d65d387a26caa803fb75a0f499d698d04
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-13 14:49:09 -0500

    exercise completed

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -3,20 +3,41 @@ let DOM = {};
 let cardFrame = document.getElementById('card-frame');
 
 function writeToDOM(data, id) {
-	let productsArr = data.products.products,
-	typesArr = data.types.types,
-	categoriesArr = data.categories.categories;
-	// STATUS:  data is loading into new object
-	// figure out how to match ids from one object to id of another
-	//to rpint category and type names
+	let productsObj = data.products.products,
+	typesObj = data.types.types,
+	categoriesObj = data.categories.categories,
+	productsArr = [], descriptionsArr = [], typesArr = [],
+	categoriesArr = [];
+	let content = '';
+	cardFrame.innerHTML = '';
 
-
+	productsObj.forEach((product) => {
+		for (var key in product) {
+			if (product[key].id == id){
+				productsArr.push(product[key].name);
+				typesArr.push(typesObj[product[key].type].name);
+				categoriesArr.push(categoriesObj[product[key].id].name);
+				descriptionsArr.push(product[key].description);
+			}
+		}
+	});
+	let contentDiv = document.createElement('div');
+	for (let i = 0; i < productsArr.length; i++) {
+		content += `
+				<div class="col-sm-3 card">
+				<p><strong>${productsArr[i]}</strong></p>
+				<p>${typesArr[i]}</p>
+				<p>${categoriesArr[i]}</p>
+				<p>${descriptionsArr[i]}</p>
+				</div>
+		`;
+	}
+	contentDiv.innerHTML = content;
+	cardFrame.appendChild(contentDiv);
 }
 
 DOM.loadData = (data, id)=> {
 	writeToDOM(data, id);
 };
-
-
 
 module.exports = DOM;
